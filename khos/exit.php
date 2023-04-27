874ddb9f21f8f139e3806828b2ef7b55ab3572cc
lugialo <gabrielzin911@gmail.com>
2023-04-27 15:19:46 -0300

    ae

--- khos/exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['email_usuario']);
-    unset($_SESSION['usuario_senha']);
-    header("Location: index.html");
-
-?>
--- khos/testes.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$timezone = new DateTimeZone('America/Sao_Paulo');
-$agora = new DateTime('now', $timezone);
-
-echo $agora->format('Y-m-d H:i:s');
-
-?>
